70ba7a9918f6fd0138e8b7bf1c6b1941e96908a7
Nahej <nahejlefebvre@gmail.com>
2019-09-27 14:28:19 -0400

    Setup small file fetching

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const http = require('http');
+//small function to make my life easier
+const log = e => { console.log(e); return e }
+//required functions
+const { createServer } = require('http');
+const { existsSync, lstatSync, readFileSync} = require('fs')
+const { parse } = require('url')
+const { extname } = require('path')
+//port on which the server will run
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+createServer((req, res) => {
+
+	//file queried
+	let pathname = parse(req.url).pathname.substring(1)
+
+	//add proper header for file type sent
+	switch( extname( pathname )){
+		case '.html':
+			res.setHeader('Content-Type', 'text/html')
+			break;
+	}
+
+	//Try get file, write file, set status... Really not efficient but good for now
+	if( existsSync(pathname) && lstatSync(pathname).isFile() ){
+		res.write( readFileSync(pathname) )
+		res.statusCode = 200
+	}
+	else res.statusCode = 404
+
+  	res.end()
+
+}).listen( port )
